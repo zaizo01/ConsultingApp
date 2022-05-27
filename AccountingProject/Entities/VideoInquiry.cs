aebf4a0f64af6711b2b6e5d9bd2afcb6c92e5ea9
Cristopher Zaiz Ortega <cristopher.zaiz@digeig.gob.do>
2022-05-27 17:51:32 +0200

    * VideoInquiry.

---
+++ AccountingProject/Entities/VideoInquiry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountingProject.Entities
+{
+    public class VideoInquiry
+    {
+        public Guid Id { get; set; }
+        public Guid PatientId { get; set; }
+        public Patient Patient { get; set; }
+        public Guid DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public string VideoInquiryPerson { get; set; }
+        public string VideoInquiryPersonFirstname { get; set; }
+        public string VideoInquiryPersonLastName { get; set; }
+
+    }
+}
